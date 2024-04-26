c1673b2be313df2a0ba9a6cd394b448137fcaf3e
김채민 <dktjdej25@gmail.com>
2024-04-26 14:21:07 +0900

    first commit

---
+++ src/main/java/com/aleph/clmmunity/ClmmunityApplication.java
@@ -0,0 +1,13 @@
+package com.aleph.clmmunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClmmunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClmmunityApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/aleph/clmmunity/MainController.java
@@ -0,0 +1,15 @@
+package com.aleph.clmmunity;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String index() {
+        return "Hello World";
+    }
+}
